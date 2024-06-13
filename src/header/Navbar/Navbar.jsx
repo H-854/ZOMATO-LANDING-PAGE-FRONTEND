0865edde44f0ea3099a7e3560186f8872d929c21
H854 <ranahritanjay@gmail.com>
2024-06-13 20:42:49 +0530

    some improvement in UI navbar

--- src/header/Navbar/Navbar.jsx
+++ src/header/Navbar/Navbar.jsx
@@ -4,17 +4,17 @@ export default function Navbar(){
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" style={{border: "2px solid white"}}>
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <p style={{color: "white"}} className="collapse navbar-collapse offset-2 mt-2">Get the app</p>
                 <div className="ms-auto w-50 contain">
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav ms-5">
-                            <p style={{color: "white"}} className="ms-5">Investor Relation</p>
-                            <p style={{color: "white"}} className="ms-5">Add Restaurant</p>
-                            <p style={{color: "white"}} className="ms-5">Login</p>
-                            <p style={{color: "white"}} className="ms-5">Sign up</p>
+                        <div className="navbar-nav ms-5 ">
+                            <p style={{color: "white"}} >Investor Relation</p>
+                            <p style={{color: "white"}} >Add Restaurant</p>
+                            <p style={{color: "white"}} >Login</p>
+                            <p style={{color: "white"}} >Sign up</p>
                         </div>
                     </div>
                 </div>
--- src/header/Search Box/SearchBox.jsx
+++ src/header/Search Box/SearchBox.jsx
@@ -3,11 +3,11 @@ import "./SearchBox.css"
 export default function SearchBox(){
     return (
         <div class="input-group w-50 offset-3 mt-3">
-            <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style={{border: "1px solid white",color: "white",width: "10rem"}}>Location</button>
+            <button class="btn btn-outline-secondary dropdown-toggle mb-5" type="button" data-bs-toggle="dropdown" aria-expanded="false" style={{border: "1px solid white",color: "white"}}>Location</button>
             <ul class="dropdown-menu">
                 <li><a class="dropdown-item" href="#">Detect Current Location</a></li>
             </ul>
-            <input type="text" class="form-control" aria-label="Text input with dropdown button" style={{height: "3rem"}} placeholder="Search for restaurant,cuisine or a dish"/>
+            <input type="text" class="form-control mb-5" aria-label="Text input with dropdown button" style={{height: "3rem"}} placeholder="Search for restaurant,cuisine or a dish"/>
         </div>
         
     )
